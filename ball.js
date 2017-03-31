76533e0fb31188d27726643915e26bf0c80fe331
barrtk <bartlomiejhawrylo@gmail.com>
2017-03-31 13:00:51 +0200

    Add files via upload

--- ball.js
+++ ball.js
@@ -2,10 +2,19 @@ var r = 24;
 var ballColor = 255;
 function Ball() {
 
+
+
       this.location = createVector(width/2, height/2);
       this.velocity = createVector(random(-5,5),random(-5,5));
-
       this.topspeed = 15;
+      var angle = random(-4/PI,4/PI);
+
+      this.velocity.x = 5 * cos(angle);
+      this.velocity.y = 5 * sin(angle);
+
+      if (random(1) < 0.5) {
+        this.velocity.x *= -1;
+      }
 
       if(this.velocity.x>0 && this.velocity.y>0 ){
         this.acceleration = createVector(0.001,0.001);
@@ -14,8 +23,9 @@ function Ball() {
       }else if(this.velocity.x>0 && this.velocity.y<0){
         this.acceleration = createVector(0.001,-0.001);
       }else if(this.velocity.x<0 && this.velocity.y>0){
-        this.acceleration = createVector(0.001,-0.001);
+        this.acceleration = createVector(-0.001,0.001);
       }
+
 
 
   this.update = function(){
@@ -37,23 +47,26 @@ function Ball() {
 
     if (this.location.y < 0+r/2) {
       this.velocity.y *= -1;
-      this.acceleration.y *=-1;
+      this.acceleration.x *= -1;
+
     }
     else if (this.location.y > height-r/2) {
       this.velocity.y *= -1;
-      this.acceleration.y *=-1;
+      this.acceleration.x *= -1;
+
     }
     else if (this.location.x < 0+r/2) {
       this.velocity.x *= -1;
-      this.acceleration.y *=-1;
+      this.acceleration.x *= -1;
     }
     else if (this.location.x > width-r/2) {
       this.velocity.x *= -1;
-      this.acceleration.y *=-1;
+      this.acceleration.x *= -1;
     }
 
   };
   this.respawn = function(){
+
     if (this.location.x < 0+r/2) {
       this.location.x = width/2;
       this.location.y = height/2;
